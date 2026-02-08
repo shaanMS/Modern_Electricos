d41df76b6fdd8b3e904e435ed1e642785b17fdd3
shaanMS <96082415+shaanMS@users.noreply.github.com>
2026-02-08 23:48:56 +0530

    just onboarded

--- modern_electricos/projectUrls/publicUrls.py
+++ modern_electricos/projectUrls/publicUrls.py
@@ -17,9 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from projectUrls import (publicUrls , privateUrls)
-
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
