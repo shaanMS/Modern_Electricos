7956db477d38f6983230ed833f200f6c39113520
shaanMS <96082415+shaanMS@users.noreply.github.com>
2026-02-10 01:22:51 +0530

    Fix views and homepage UI

--- home/views.py
+++ home/views.py
@@ -9,6 +9,26 @@ class HomeView(TemplateView):
         context['title'] = 'Shakti Electricals'   #  like a default dictionary 
         context['tagLine'] = 'Professional Electrical Solutions'
         context['contact'] = 'Emergency: (555) 123-4567'
+        context['heroSubtitle'] = 'PowerPro Electrical delivers certified, safe, and reliable electrical services with over 15 years of industry experience. Fully licensed and insured for your peace of mind.'
+        context[''] = ''
+        context[''] = ''
+        context[''] = ''
+        context[''] = ''
+        context[''] = ''
+        context[''] = ''
+        context[''] = ''
+        context[''] = ''
+        context[''] = ''
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
         return context
 
 
